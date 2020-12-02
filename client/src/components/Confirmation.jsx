169adc82dc7eca4ed905b3cf84b1a2e5cbcf1306
Carrie Guan <carrieguan17@gmail.com>
2020-12-01 17:57:34 -0800

    confirmation styling complete

--- client/src/components/Confirmation.jsx
+++ client/src/components/Confirmation.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import styles from '../styles/Confirmation.css';
 
 function Confirmation (props) {
   return (
-    <div>
-      <h1>Confirmation</h1>
-      <div>Thank you for joining Leetcall. Below is your profile details. If you have any questions, feel free to contact us for futher assistance.</div>
-      <div>First Name: {props.userData.firstName}</div>
-      <div>Last Name: {props.userData.lastName}</div>
-      <div>Gender: {props.userData.gender}</div>
-      <div>Date of Birth: {props.userData.dateOfBirth}</div>
-      <div>Phone Number: {props.userData.phoneNumber}</div>
+    <div className={styles.confirmationContainer}>
+      <div className={styles.confirmationHeader}>Confirmation</div>
+      <div className={styles.confirmationContent}>
+        <p className={styles.confirmationMessage}>Thank you for joining LeetCall. Below is your profile information. If you have any questions, feel free to contact us for futher assistance.</p>
+        <div className={styles.userData}>
+          <div>First Name: {props.userData.firstName}</div>
+          <div>Last Name: {props.userData.lastName}</div>
+          <div>Gender: {props.userData.gender}</div>
+          <div>Date of Birth: {props.userData.dateOfBirth}</div>
+          <div>Phone Number: {props.userData.phoneNumber}</div>
+        </div>
+      </div>
     </div>
   )
 }
